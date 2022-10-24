ff565b0c8c72a0b7dbddf2ec09c7ab43cd4b508b
mehedy3107 <mhador111@gmail.com>
2022-10-25 02:22:19 +0600

    Sikho Sobai

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#aa192a",
+          secondary: "#b25629",
+          accent: "#21ff67",
+          neutral: "#201826",
+          "base-100": "#E8EBED",
+          info: "#23BBE1",
+          success: "#17A698",
+          warning: "#F1BF41",
+          error: "#F03B38",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
